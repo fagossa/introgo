6893e17ed6fd230be930a701238b6e49c2414fc8
Fabian Gutierrez <fagossa@gmail.com>
2017-08-06 18:12:47 +0200

    reformat to improble readability

--- katas.go
+++ katas.go
@@ -6,11 +6,22 @@ import "supermarket"
 func main() {
 	fmt.Println("--- Supermarket Price ---")
 	var basket = make([]supermarket.ProductSelection, 0)
-	basket = append(basket, supermarket.ProductSelection{Amt: 3, Prod: supermarket.Product{Price: 10, Name: "Meat"}})
-	basket = append(basket, supermarket.ProductSelection{Amt: 4, Prod: supermarket.Product{Price: 20, Name: "Cheese"}})
+	basket = append(basket, supermarket.ProductSelection{
+		Amt: 3,
+		Prod: supermarket.Product{
+			Price: 10,
+			Name:  "Meat"}})
+	basket = append(basket, supermarket.ProductSelection{
+		Amt: 4,
+		Prod: supermarket.Product{
+			Price: 20,
+			Name:  "Cheese"}})
 
 	b := supermarket.Basket{Contents: basket}
 
-	fmt.Println("One Free Discount=", supermarket.CalculatePrice(b, supermarket.OneFreeDiscountStrategy))
-	fmt.Println("No Discount=", supermarket.CalculatePrice(b, supermarket.NoDiscountStrategy))
+	fmt.Println("One Free Discount=",
+		supermarket.CalculatePrice(b, supermarket.OneFreeDiscountStrategy))
+
+	fmt.Println("No Discount=",
+		supermarket.CalculatePrice(b, supermarket.NoDiscountStrategy))
 }
